c3eee82a45e4a066cc488f4e9c26eafed74ad9e4
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-11-06 20:12:32 +0100

    reactive

--- src/app/modules/manager/pages/bb-news/pages/approved-news/approved-news.component.ts
+++ src/app/modules/manager/pages/bb-news/pages/approved-news/approved-news.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import { switchMap } from "rxjs";
+import { Component } from "@angular/core";
+import { Observable, Subject, switchMap } from "rxjs";
 import { BB_NewsService } from "../../services/bb-news.service";
 
 @Component({
   templateUrl: './approved-news.component.html'
 })
-export class ApprovedNewsComponent implements OnInit {
+export class ApprovedNewsComponent {
+  m_Disapprove$: Subject<number> = new Subject<number>().pipe(
+    switchMap((id: number) => {
+      return this.m_BBNewsService.patchNewsState(id, 'disapprove').pipe(
+        switchMap(_ => this.m_BBNewsService.fetchNews('approved'))
+      )
+    })
+  ) as Subject<number>;
+  m_FetchNews$: Observable<any> = this.m_BBNewsService.fetchNews('approved');
 
   constructor(private m_BBNewsService: BB_NewsService) { }
   
-  ngOnInit(): void {
-    this.m_BBNewsService.fetchNews('approved').subscribe();
-  }
-
-  disapprove(id: number): void {
-    this.m_BBNewsService.patchNewsState(id, 'disapprove').pipe(
-      switchMap(_ => this.m_BBNewsService.fetchNews('approved'))
-    ).subscribe();
-  }
 }
--- src/app/modules/manager/pages/bb-news/pages/unchecked-news/unchecked-news.component.ts
+++ src/app/modules/manager/pages/bb-news/pages/unchecked-news/unchecked-news.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from "@angular/core";
-import { switchMap } from "rxjs";
+import { Component } from "@angular/core";
+import { Observable, Subject, switchMap } from "rxjs";
 import { BB_NewsService } from "../../services/bb-news.service";
 
 @Component({
   templateUrl: './unchecked-news.component.html'
 })
-export class UncheckedNewsComponent implements OnInit {
+export class UncheckedNewsComponent {
+  m_Disapprove$: Subject<number> = new Subject<number>().pipe(
+    switchMap((id: number) => {
+      return this.m_BBNewsService.patchNewsState(id, 'disapprove').pipe(
+        switchMap(_ => this.m_BBNewsService.fetchNews('unchecked'))
+      )
+    })
+  ) as Subject<number>;
+
+  m_Approve$: Subject<number> = new Subject<number>().pipe(
+    switchMap((id: number) => {
+      return this.m_BBNewsService.patchNewsState(id, 'approve').pipe(
+        switchMap(_ => this.m_BBNewsService.fetchNews('unchecked'))
+      )
+    })
+  ) as Subject<number>;
+  m_FetchNews$: Observable<any> = this.m_BBNewsService.fetchNews('unchecked');
 
   constructor(private m_BBNewsService: BB_NewsService) { }
   
-  ngOnInit(): void {
-    this.m_BBNewsService.fetchNews('unchecked').subscribe();
-  }
-  
-  approve(id: number): void {
-    this.m_BBNewsService.patchNewsState(id, 'approve').pipe(
-      switchMap(_ => this.m_BBNewsService.fetchNews('unchecked'))
-    ).subscribe();
-  }
-
-  disapprove(id: number): void {
-    this.m_BBNewsService.patchNewsState(id, 'disapprove').pipe(
-      switchMap(_ => this.m_BBNewsService.fetchNews('unchecked'))
-    ).subscribe();
-  }
 }
