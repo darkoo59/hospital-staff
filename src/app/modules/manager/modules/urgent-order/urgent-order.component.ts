980f1fa8d350154b158f0c4744456ef7751f19c3
VojinBjelica <vojinb111@gmail.com>
2022-12-11 04:18:57 +0100

    Call backend api upon form submit

--- src/app/modules/manager/modules/urgent-order/urgent-order.component.ts
+++ src/app/modules/manager/modules/urgent-order/urgent-order.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, UntypedFormGroup, Validators } from '@angular/forms';
-import { take } from 'rxjs';
+import { catchError, EMPTY, take } from 'rxjs';
 import { BloodBank } from './model/blood-bank.model';
+import { UrgentOrderDTO } from './model/urgent-order-dto';
 import { UrgentOrderService } from './services/urgent-order.service';
 
 @Component({
@@ -32,7 +33,35 @@ export class UrgentOrderComponent implements OnInit {
 
   onSubmit() : void {
     this.m_Errors.length = 0;
+
+    var entireServer = this.form.get('bb-select')?.value
+
+    const dto: UrgentOrderDTO = {
+      bloodType: this.form.get('blood-type-select')?.value,
+      quantity: this.form.get('quantity')?.value,
+      server: entireServer.server
+    }
+
+    console.log(dto.bloodType + " " + dto.quantity + " " + dto.server)
+
+    this.form.updateValueAndValidity();
+    if (!this.form.valid)  return;
     
+    this.service.OrderUrgently(dto)
+      .pipe(catchError(res => {
+        console.log(res);
+        const error = res.error;
+        if(error && error.message) {
+          this.m_Errors.push(error.message);
+          return EMPTY;
+        }
+        this.m_Errors.push(error);
+        return EMPTY;
+      }))
+      .subscribe(data => {
+        console.log(dto.bloodType + " " + dto.quantity + " " + dto.server)
+      });
+  
   }
 
 }
